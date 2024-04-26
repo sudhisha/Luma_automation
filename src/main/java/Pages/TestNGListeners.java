941f26223ab2c7484f850054361a47320336e85a
Sudhisha <sudhishapavithran20@gmail.com>
2024-04-27 01:00:53 +0530

    LumaAutomation

--- src/main/java/Pages/TestNGListeners.java
+++ src/main/java/Pages/TestNGListeners.java
@@ -1,17 +1,17 @@
 package Pages;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+//import org.apache.commons.io.FileUtils;
+//import org.openqa.selenium.OutputType;
+//import org.openqa.selenium.TakesScreenshot;
+//import org.openqa.selenium.WebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Date;
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//import java.util.Date;
 
 public class TestNGListeners implements ITestListener {
     @Override
