55149e38111975db5c890e894675b77179830d26
Sudhisha <sudhishapavithran20@gmail.com>
2024-04-29 19:03:11 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/MainClass_Pages/Main_Account.java
+++ src/main/java/MainClass_Pages/Main_Account.java
@@ -27,11 +27,53 @@ public class Main_Account {
         JavascriptExecutor js =(JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(150,200)");
         myacc.click_edit_link();
-        System.out.println("testing");
+        js.executeScript("window.scrollBy(300,350)");
+        myacc.verify_firstname("Deepika");
+        myacc.verify_lastname("K");
+        myacc.click_change_email();
+        myacc.verify_email("deepikak1996@gmail.com");
+        myacc.change_email("deepikak96@gmail.com");
+        myacc.enter_current_password("Deepikak@1996");
+        myacc.Click_save();
+        myacc.verify_login_page_title("Customer Login");
+        myacc.verify_success_message("You saved the account information.");
     }
-    @AfterTest
-    public void CloseDriver() throws InterruptedException {
-        Thread.sleep(3000);
-        driver.close();
+    @Test(priority = 2)
+    public void change_password_of_account() {
+        MyAccount myacc = new MyAccount(driver);
+        myacc.login_with_valid_inputs();
+        myacc.click_customer_dropdown();
+        myacc.click_Myaccount();
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(150,200)");
+        myacc.click_edit_link();
+        js.executeScript("window.scrollBy(300,350)");
+        myacc.verify_firstname("Deepika");
+        myacc.verify_lastname("K");
+        myacc.Click_change_password();
+        myacc.enter_current_password("Deepikak@1996");
+        myacc.enter_new_password("Deepikak@199");
+        myacc.enter_confirm_password("Deepikak@199");
+        myacc.Click_save();
+        myacc.verify_login_page_title("Customer Login");
+        myacc.verify_pswd_success_message("You saved the account information.");
     }
+    @Test(priority = 3)
+    public void add_address() {
+        MyAccount myacc = new MyAccount(driver);
+        myacc.login_with_valid_inputs();
+        myacc.click_customer_dropdown();
+        myacc.click_Myaccount();
+        myacc.Click_addressBook();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("window.scrollBy(400,500)");
+        myacc.verify_firstname("Deepika");
+        myacc.verify_lastname("K");
+
+    }
+//    @AfterTest
+//    public void CloseDriver() throws InterruptedException {
+//        Thread.sleep(3000);
+//        driver.close();
+//    }
 }
